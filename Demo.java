dcf9873fe933d51dff27c16561e2293c3610760e
Chirag <himselfchirag@gmail.com>
2025-01-22 13:48:53 +0530

    in the middle of the class

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ class test
 {
     int x = 10;
     int y = 20;
-    
+    static int j = 87;
     void show()
     {
         int z = 30;
@@ -15,18 +15,20 @@ class test
 public class Demo {
     public static void main(String[] args) {
         test t1 = new test();
+        //if you want to access the static variables inside the main or any other class you have to use class name in which the variable is decealred
+        System.out.println(test.j);
         t1.show();
     }
 }
 
 
 // class Demo{
-//     // int x = 10;
+//     int x = 10;
 //     static int y = 20; //only static variables can be called from a static class
 //     public static void main(String[] args) {
 //         int z = 30;
 //         System.out.println("Z is : "+ z);
-//         // System.out.println("x is : "+ x);
+//         System.out.println("x is : "+ (new Demo().x)); //the non static methods or members can be accessd thorugh the objects or with help of object refrernces
 //         System.out.println("y is : "+ y); 
 //     }
 // }
