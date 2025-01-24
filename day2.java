05d614a263f697f1dbc10ad6319036f585b79cf3
Chirag <himselfchirag@gmail.com>
2025-01-24 16:13:12 +0530

    done with day

---
+++ day2.java
@@ -0,0 +1,17 @@
+public class day2 {
+    public static void main(String[] args) {
+        /*****************************************************WRAPPER CLASS**********************************************/
+
+        Integer a = new Integer(56);
+        System.out.println(a);
+
+        /*****************************************************Operators**********************************************/
+        System.out.println(25-5.0);
+        System.out.println(25*5.0);
+        System.out.println(25/5.0);
+        System.out.println(25+5.0);
+        System.out.println(25%5.0);
+
+    }   
+}
+
