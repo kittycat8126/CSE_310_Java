512b7cf6d2ee20366c79ce7c1640080137180a5e
Chirag <himselfchirag@gmail.com>
2025-01-31 16:16:22 +0530

    masala

--- day4.java
+++ day4.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class day4 {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
+        System.out.println("Enter two numbers to divide: ");
         int k = n.nextInt();
         int y = n.nextInt();
-        int div = k/y;
-        System.out.println("Division of "+k+" and "+y+" is "+div);
-    }
-    
+        if(k==0 || y ==0)
+        {
+            throw new ArithmeticException("Division by zero is not possible Buddy");
+        }
+        try {
+            int div = k/y;
+            System.out.println("Division of "+k+" and "+y+" is "+div);
+        } catch (Exception e) {
+            System.out.println("Exception: "+e);
+        }
+    }    
 }
