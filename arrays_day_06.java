f761132243124979c9ddd60e5f78d8fb63e2544a
Chirag <himselfchirag@gmail.com>
2025-02-03 15:12:50 +0530

    ye kuch nhi padha rhe

---
+++ arrays_day_06.java
@@ -0,0 +1,9 @@
+public class arrays_day_06 {
+    public static void main(String[] args) {
+        double [] marks = {12.3,45.6,78.9,34.54,35.6,13.9};
+        for(double i : marks)
+        {
+            System.out.print(i+" ");
+        }
+    }    
+}
--- day5.java
+++ day5.java
@@ -1,28 +1,37 @@
 import java.util.Scanner;
-
 public class day5 {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
-        double att_per= n.nextDouble();
-        if(att_per>90)
-        {
-            System.out.println("Bonus marks : "+ 5);
+        System.out.println("Enter attendace percentage: ");
+        double att_per = n.nextDouble();
+        System.out.println("Bonus marks : " + 
+            (att_per > 90 ? 5 : att_per >= 85 ? 4 : att_per >= 80 ? 3 : att_per >= 75 ? 2 : 0));
+        String x = "yoyo";
+        switch (x) {
+            case "yaya":
+                System.out.println("did not worked");
+                break;
+            case "yoyo":
+                System.out.println("worked");
+                break;
+            default:
+                break;
         }
-        else if(att_per>=85 && att_per<90)
+        System.out.println("Enter number");
+        int tdn = n.nextInt();
+        int rev = 0;
+        while(tdn!=0)
         {
-            System.out.println("Bonus marks: "+4);
+            int ld = tdn%10;
+            tdn /= 10;
+            rev = rev*10+ld;
         }
-        else if(att_per>=80 && att_per<85)
+        while(rev != 0)
         {
-            System.out.println("Bonus marks: "+3);
+            System.out.println(rev%10);
+            rev = rev /10;
         }
-        else if(att_per>=75 && att_per<80)
-        {
-            System.out.println("Bonus marks: "+2);
-        }
-        else
-        {
-            System.out.println("Bonus marks: "+0);
-        } 
+        
+        
     }
 }
