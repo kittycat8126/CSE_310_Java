aff5e349717d29ea9e9b0cecb44b632f81cef488
Chirag <himselfchirag@gmail.com>
2025-02-14 15:53:30 +0530

    yo

--- Strings/string.java
+++ Strings/string.java
@@ -4,7 +4,12 @@ class string
         String a = "Aswin";
         String b = "Kumar";
         String c = "Aswin";
+        String d = new String("Aswin");
         System.out.println(a + "," + b + "," + c);
-        System.out.println(a.length()); 
+        System.out.println(a.length());
+        System.out.println(a==c);
+        System.out.println(a.equals(c));
+        System.out.println(d==a);
+        System.out.println(d.equals(a));
     }
 }
