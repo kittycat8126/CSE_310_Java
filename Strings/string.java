adbd73f144104b0b24efe5c5be7dcf06cff1f656
Chirag <himselfchirag@gmail.com>
2025-02-14 15:36:43 +0530

    yo

---
+++ Strings/string.java
@@ -0,0 +1,9 @@
+class string
+{
+    public static void main(String[] args) {
+        String a = "Aswin";
+        String b = "Kumar";
+        String c = "Aswin";
+        System.out.println(a + "," + b);
+    }
+}
