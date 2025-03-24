57571cd072dd98a683051467931095074de578d1
Chirag <himselfchirag@gmail.com>
2025-03-24 11:56:19 +0530

    yo

---
+++ ExceptionHandling/userdefinedexceptionclass.java
@@ -0,0 +1,12 @@
+class Myexception extends Exception{
+    Myexception(int x){
+        super("My exception occured");
+    }
+}
+
+
+public class userdefinedexceptionclass {
+    public static void main(String[] args) {
+        
+    }
+}
