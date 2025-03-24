d54d0d0974cd74e21a7db71b24e69087ead970c9
Chirag <himselfchirag@gmail.com>
2025-03-24 11:31:12 +0530

    exc hand

--- ExceptionHandling/exchan_01.java
+++ ExceptionHandling/exchan_01.java
@@ -9,25 +9,44 @@
  *               }
  */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
+class x extends Exception {
+    x(String s){
+        super(s);
+    }
+    @Override
+    public String getMessage(){
+        return "Hello vinay ! this side class X you have successfully overrided the method getMessage()";
+    }
+}
 public class exchan_01 {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
         int a,b,c=(int)Math.random();
-        boolean falg = true;
-        System.out.println("Enter two numbers: ");
-        a = n.nextInt();
-        b = n.nextInt();
+        
         try{
-            c = a/b;  
+            System.out.println("Enter two numbers: ");
+            a = n.nextInt();
+            b = n.nextInt();
+            //manually throwing exception
+            if(b==0){
+                throw new x("Divide by zero exception");
+            }
+            c = a/b;
         }
-        catch(Exception e){
-            System.out.println("Can not divide by zero");
-            falg = false;
+        catch(x e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getLocalizedMessage());
+            return;
         }
-        if(falg)
-        System.out.println("Result: "+c);
+        catch(ArithmeticException e){
+            System.out.println("Its not me its you!");
+        }
+        finally{
+            n.close();
+            System.out.println("Finally block executed");
+        }
     }
 }
 
