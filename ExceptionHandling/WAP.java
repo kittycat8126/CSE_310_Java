94538afe1833a36efbf81bcf0182c2aba192306b
Chirag Gangwar <himselfchirag@gmail.com>
2025-03-28 16:26:54 +0530

    yo

--- ExceptionHandling/WAP.java
+++ ExceptionHandling/WAP.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
-
+import java.io.IOException;
+import java.io.InputStream;
 public class WAP {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int k;
-        float l;
-        System.out.println("Enter the integer: ");
-        k = s.nextInt();
-        System.out.println("Enter the float");
-        l = s.nextFloat();
-        System.out.println("Integer is : "+k+"\nFloat is : "+l);
+    public static void main(String[] args) throws IOException {
+        int k = System.in.read(); //read returns ASCII value of input. //read 1 bute from input
+        //System.out.println(k);
+        byte[] d = new byte[5];
+        // int l = System.in.read(d); //reads nest byte from input
+        // for(int f: d){
+        //     System.out.print(f+" ");
+        // }
+        
+        int o = System.in.read(d,0,3); //reads next 3 bytes into array 0,1,2
+        for(int f: d){
+            System.out.print(f+" ");
+        }
     }
 }
