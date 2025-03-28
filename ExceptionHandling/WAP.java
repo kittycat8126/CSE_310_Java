3fef71e68768c4427e0b727d7564fbfd9ac238ec
Chirag Gangwar <himselfchirag@gmail.com>
2025-03-28 15:30:40 +0530

    yo

---
+++ ExceptionHandling/WAP.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class WAP {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int k;
+        float l;
+        System.out.println("Enter the integer: ");
+        k = s.nextInt();
+        System.out.println("Enter the float");
+        l = s.nextFloat();
+        System.out.println("Integer is : "+k+"\nFloat is : "+l);
+    }
+}
