3be19d09e9c348fd126d90d30086dfa7c59e42fb
Chirag <himselfchirag@gmail.com>
2025-04-07 15:21:17 +0530

    yo

--- filehandling/filehandling/classes.java
+++ filehandling/filehandling/classes.java
@@ -1,23 +1,26 @@
 import java.io.*;
+
+class Money{
+    
+}
 public class classes {
     public static void main(String[] args) throws Exception{
         FileOutputStream fos = new FileOutputStream("classes.txt");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeBytes("Apples are sweet");
         oos.writeBytes("apples are my fav");
-        oos.writeInt(69);
-        oos.writeDouble(10.11);
+        oos.writeObject(new Integer(69));
+        oos.writeObject(new Double(10.11));
         oos.close();
         fos.close();
         System.out.println("File succesfully saved!!!!");
 
         FileInputStream fis = new FileInputStream("classes.txt");
         ObjectInputStream ois = new ObjectInputStream(fis);
-        System.out.println("readLine");
-        String a = ois.readObject().toString();
-        int i = ois.readInt();
-
-        System.out.println(a);
+        String s1 = ois.readObject().toString();
+        String s2 = ois.readObject().toString();
+        Integer a = (Integer)ois.readObject();
+        Double o = (Double)ois.readObject();
         fis.close();
         ois.close();
     }
